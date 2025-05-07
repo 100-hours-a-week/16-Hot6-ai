b8a4ff23b4503f4b76bc862617df44129426b8b2
thplus <glanz6670@naver.com>
2025-05-07 21:30:21 +0900

    feat: low_cpu_mem 추가

--- app/services/txt2img.py
+++ app/services/txt2img.py
@@ -30,6 +30,7 @@ class TextToImage:
             torch_dtype = torch.float16,
             variant = "fp16",
             use_safetensors = True,
+            low_cpu_mem_usage=True
         )
         self.pipe.to("cuda")
 
