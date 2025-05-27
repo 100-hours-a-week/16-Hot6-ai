26b189697316e1b275ee45ff3752412c846cc342
thplus <glanz6670@naver.com>
2025-05-27 16:39:29 +0900

    fix: s3 local 파일 불러오도록 수정

--- app/utils/s3.py
+++ app/utils/s3.py
@@ -1,4 +1,5 @@
-from io import BytesIO
+# from io import BytesIO
+import os
 import boto3
 import uuid
 from core.config import settings
@@ -14,20 +15,31 @@ class S3:
             aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY,
         )
 
-    def save_s3(self, image):
-        buffer = BytesIO()
-        image.save(buffer, format = "PNG")
-        buffer.seek(0)
+    def save_s3(self, image_path):
+        # buffer = BytesIO()
+        # image.save(buffer, format = "PNG")
+        # buffer.seek(0)
+
+        if not os.path.isfile(image_path):
+            raise FileNotFoundError(f"{image_path} 해당 경로에 이미지 파일이 존재하지 않습니다.")
 
         unique_id = str(uuid.uuid4())
         s3_key = f"assets/images/{unique_id}.png"
 
-        self.s3_client.upload_fileobj(
-            buffer,
-            settings.S3_BUCKET_NAME,
-            s3_key,
-            ExtraArgs={"ContentType": "image/png"}
-        )
+        with open(image_path, "rb") as f:
+            self.s3_client.upload_fileobj(
+                f,
+                settings.S3_BUCKET_NAME,
+                s3_key,
+                ExtraArgs={"ContentType": "image/png"}
+            )
+
+        # self.s3_client.upload_fileobj(
+        #     buffer,
+        #     settings.S3_BUCKET_NAME,
+        #     s3_key,
+        #     ExtraArgs={"ContentType": "image/png"}
+        # )
 
         generated_image_url = f"https://img.onthe-top.com/{unique_id}.png"
         logger.info(f"생성된 이미지 URL = {generated_image_url}")
