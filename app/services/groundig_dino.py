53f12631125860ee305f647169375298dec88741
thplus <glanz6670@naver.com>
2025-06-13 20:39:04 +0900

    feat: try, except

--- app/services/groundig_dino.py
+++ app/services/groundig_dino.py
@@ -48,31 +48,35 @@ class GroundingDINO:
             logger.error(f"Grounding DINO is failed: {e}")
 
     def labeling(self, labels, image_path):
-        txt = ". ".join(labels) + "."
-        image = Image.open(image_path)
+        try:
+            txt = ". ".join(labels) + "."
+            image = Image.open(image_path)
 
-        inputs = self.processor(images=image, text=txt, return_tensors="pt").to("cuda")
+            inputs = self.processor(images=image, text=txt, return_tensors="pt").to("cuda")
 
-        with torch.no_grad():
-            outputs = self.dino(**inputs)
+            with torch.no_grad():
+                outputs = self.dino(**inputs)
 
-        results = self.processor.post_process_grounded_object_detection(
-            outputs,
-            inputs.input_ids,
-            box_threshold=0.4,
-            text_threshold=0.3,
-            target_sizes=[image.size[::-1]]
-        )[0]
+            results = self.processor.post_process_grounded_object_detection(
+                outputs,
+                inputs.input_ids,
+                box_threshold=0.4,
+                text_threshold=0.3,
+                target_sizes=[image.size[::-1]]
+            )[0]
 
-        boxes = results["boxes"].cpu().numpy().astype(int)
-        labels = results["labels"]
-        
-        label_to_centers = defaultdict(list)
+            boxes = results["boxes"].cpu().numpy().astype(int)
+            labels = results["labels"]
+            
+            label_to_centers = defaultdict(list)
 
-        for label, box in zip(labels, boxes):
-            x1, y1, x2, y2 = box
-            cx = int((x1 + x2) / 2)
-            cy = int((y1 + y2) / 2)
-            label_to_centers[label].append((cx, cy))
+            for label, box in zip(labels, boxes):
+                x1, y1, x2, y2 = box
+                cx = int((x1 + x2) / 2)
+                cy = int((y1 + y2) / 2)
+                label_to_centers[label].append((cx, cy))
+            logger.info(f"Detected object: {label_to_centers}")
 
-        return label_to_centers
+            return label_to_centers
+        except Exception as e:
+            logger.info(f"Labeling is failed: {e}")
