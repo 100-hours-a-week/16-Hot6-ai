f870759153d8296a23a41b63cc9cce719e9d5b4a
thplus <glanz6670@naver.com>
2025-06-13 20:17:40 +0900

    feat: style change

--- app/services/sdxl_inpainting.py
+++ app/services/sdxl_inpainting.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import torch
+import numpy as np
 import random
 from core.config import settings
 import logging
@@ -10,10 +11,10 @@ class SDXL:
     def __init__(self, pipe):
         self.pipe = pipe
 
-    def generate_image(self, origin_image, mask_image, prompt, weekday_ko, prompt_2: str = None, negative_prompt: str = None):
+    def sdxl_inpainting(self, origin_image, mask_image, prompt, weekday_ko, prompt_2: str = None, negative_prompt: str = None):
         try:
             if negative_prompt is None:
-                negative_prompt = settings.NEGATIVE_PROMPT
+                negative_prompt = f"{settings.NEGATIVE_PROMPT}, {settings.weekday_neg2[weekday_ko]}, photorealistic, realistic texture"
 
             if prompt_2 is None:
                 prompt_2 = "ott_style, cinematic"
@@ -43,3 +44,39 @@ class SDXL:
         
         except Exception as e:
             logger.error(f"SDXL_Inpating is failed: {e}")
+
+    def sdxl_style(self, image_path, lora_name :str = None, lora_weight :float = None):
+        try:
+            if lora_name is None:
+                lora_name = "basic_lora"
+            if lora_weight is None:
+                lora_weight = 2.0
+
+            image = Image.open(image_path).convert("RGB")
+            mask_image = Image.fromarray(np.ones((image.height, image.width), dtype=np.uint8) * 255)
+            generator = torch.Generator(device="cuda").manual_seed(random.randint(0, 100000))
+
+            negative_prompt = settings.NEGATIVE_PROMPT
+
+            self.pipe.set_adapters([f"{lora_name}"], [{lora_weight}])
+
+            result = self.pipe(
+                prompt = "desk setup. keep the layout and objects the same, just change the art style to a semi-realistic 3D render.",
+                prompt_2 = f"{lora_name}, concept art, high detail",
+                negative_prompt = negative_prompt,
+                image = image,
+                mask_image = mask_image,
+                guidance_scale = 9.0,
+                num_inference_steps=40,
+                strength=0.95,
+                generator=generator
+            ).images[0]
+
+            save_path = "/temp/style.png"
+            result.save(save_path)
+
+            logger.info(f"Style Changed: {save_path}")
+
+            return save_path
+        except Exception as e:
+            logger.info(f"sdxl_style is failed: {e}")
