4e52978b4bb3626d415b4567b6df855f0243b67a
thplus <glanz6670@naver.com>
2025-06-13 19:33:37 +0900

    feat: class, try, except

--- app/services/groundig_dino.py
+++ app/services/groundig_dino.py
@@ -5,40 +5,44 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-def __init__(self, processor, dino):
-    self.processor = processor
-    self.dino = dino
+class GroundingDINO:
+    def __init__(self, processor, dino):
+        self.processor = processor
+        self.dino = dino
 
-def run_dino(self, path, txt=None):
-    if txt is None:
-        txt = "monitor. keyboard. mouse. laptop. speaker."
+    def run_dino(self, path, txt=None):
+        if txt is None:
+            txt = "monitor. keyboard. mouse. laptop. speaker."
+        try:
+            image = Image.open(path).convert("RGB")
+            inputs = self.processor(images=image, text=txt, return_tensors="pt").to("cuda")
 
-    image = Image.open(path).convert("RGB")
-    inputs = self.processor(images=image, text=txt, return_tensors="pt").to("cuda")
+            with torch.no_grad():
+                outputs = self.dino(**inputs)
 
-    with torch.no_grad():
-        outputs = self.dino(**inputs)
+            results = self.processor.post_process_grounded_object_detection(
+                outputs,
+                inputs.input_ids,
+                box_threshold=0.4,
+                text_threshold=0.3,
+                target_sizes=[image.size[::-1]]
+            )[0]
 
-    results = self.processor.post_process_grounded_object_detection(
-        outputs,
-        inputs.input_ids,
-        box_threshold=0.4,
-        text_threshold=0.3,
-        target_sizes=[image.size[::-1]]
-    )[0]
+            boxes = results["boxes"].cpu().numpy()
+            boxes = torch.tensor(boxes).to("cuda")
+            labels = results["text_labels"]
+            #scores = results["scores"].cpu().numpy()
 
-    boxes = results["boxes"].cpu().numpy()
-    boxes = torch.tensor(boxes).to("cuda")
-    labels = results["text_labels"]
-    #scores = results["scores"].cpu().numpy()
+            label_to_centers = defaultdict(list)
+            for label, box in zip(labels, boxes):
+                x1, y1, x2, y2 = box.tolist()
+                cx = int((x1 + x2) / 2)
+                cy = int((y1 + y2) / 2)
+                label_to_centers[label].append((cx, cy))
 
-    label_to_centers = defaultdict(list)
-    for label, box in zip(labels, boxes):
-        x1, y1, x2, y2 = box.tolist()
-        cx = int((x1 + x2) / 2)
-        cy = int((y1 + y2) / 2)
-        label_to_centers[label].append((cx, cy))
+            logger.info(f"Detected Object: {labels}")
 
-    logger.info(f"Detected Object: {labels}")
-
-    return boxes, labels, label_to_centers
+            return boxes, labels, label_to_centers
+        
+        except Exception as e:
+            logger.error(f"Grounding DINO is failed: {e}")
