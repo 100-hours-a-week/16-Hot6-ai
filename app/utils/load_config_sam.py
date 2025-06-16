af809bb69079cc38650dacbfb12adde3f033c41a
thplus <glanz6670@naver.com>
2025-06-16 21:15:45 +0900

    feat: Colab Dev Test v3

--- app/utils/load_config_sam.py
+++ app/utils/load_config_sam.py
@@ -1,11 +1,16 @@
 import os
 from hydra import initialize_config_dir
+from hydra.core.global_hydra import GlobalHydra
 from contextlib import contextmanager
 
 @contextmanager
 def hydra_config_context(config_path: str):
     config_dir = os.path.dirname(config_path)
     config_name = os.path.splitext(os.path.basename(config_path))[0]
+
+    # ✅ 기존 Hydra 인스턴스 초기화
+    if GlobalHydra.instance().is_initialized():
+        GlobalHydra.instance().clear()
 
     with initialize_config_dir(config_dir=config_dir):
         yield config_name
